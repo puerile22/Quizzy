93a49ce6fabeab90559bbd4308d879019803e623
Peng Gao <puerile22@gmail.com>
2014-10-10 15:15:20 -0700

    a user can take a quiz and show the score when it is done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,33 +17,44 @@
 // Your Quizzy code should go here.
 var template = _.template("<a href='' id='<%= id %>'><%= title %></a><br>");
 var list = "<h4><%= question %></h4><% _.each(choices,function(choice){ %> <input type='radio' name='choice' id='<%= choice %>' value='<%= choice %>'><label for='<%= choice %>'><%= choice %></label><br/>  <% }); %><button type='button'>Submit!</button><p class='answer'><%= answer %></p>";
-var answerQuestion = function(){
+var answerQuestion = function(id,num,score){
   $('button').click(function(){
     if ($('.answer').text() === $('input:checked').val()) {
       $('.main').append('<h3 class=result>Correct!</h3>');
-      setTimeout(function(){return;},1000);
+      num++;
+      score++;
+      setTimeout(function(){singleQuestion(id,num,score);},1000);
     } else {
       $('.main').append('<h3 class=result>Incorrect!</h3>');
-      setTimeout(function(){return;},1000);
+      num++;
+      setTimeout(function(){singleQuestion(id,num,score);},1000);
     }
   });
-}
+};
 var showquestion = function(id){
   $('#'+id+'').click(function(e){
     e.preventDefault();
-    $.get('/quizzes/'+id+'/questions',function(data){
-      for (var i = 0;i < data.length;i++) {
-        $('.main').empty();
-        var questionId = data[i].id;
+    singleQuestion(id,0,0);
+  });
+};
+var singleQuestion = function(id,num,score) {
+  $.get('/quizzes/'+id+'/questions',function(data){
+    $('.main').empty();
+    if (num < data.length) {
+      var questionId = data[num].id;
+      if (data[num].question_type === 'multiple') {
         $.get('/quizzes/'+id+'/questions/'+questionId+'',function(questionData){
           var question = _.template(list,{question:questionData.question,choices:questionData.choices.split(";"),answer:questionData.answer});
           $('.main').append(question);
-          answerQuestion();
+          answerQuestion(id,num,score);
         });
       }
-    });
+    } else {
+      score = (score/data.length) * 100 ;
+      $('.main').append("<h2>Final score:"+score+"</h2>")
+    }
   });
-}
+};
 $(document).ready(function(){
   $.get('/quizzes',function(data){
     for (var i = 0;i<data.length;i++) {
