957266ae926b15c9f1ca4a01e919eddd0c98592b
Peng Gao <puerile22@gmail.com>
2014-10-11 20:21:47 -0700

    add new question to a quiz with different question types

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require underscore
 // Your Quizzy code should go here.
-var template = _.template("<a href='' id='<%= id %>'><%= title %></a><br>");
+var template = _.template("<a href='' id='<%= id %>'><%= title %></a>     <a href='' class='<%= id %>'>Add new question</a><br>");
 var answerQuestion = function(id,num,score){
   $('button').click(function(){
     if ($('.answer').text() === $('input:checked').val()) {
@@ -108,7 +108,101 @@ var showQuiz = function() {
         id: data[i].id
       });
       $('.main').append(quizz);
+      $('.'+data[i].id+'').click(function(e){
+        e.preventDefault();
+        var quizId = $(this).attr('class');
+        // var quizId = data[i].id;
+        $('.main').empty();
+        $('.main').append("<h2>Add new question</h2><label for='newquestion'>Question:</label><br><input type='text' id='newquestion'><br><label for='type'>Question type:</label><br><select id='type'><option>multiple</option><option>boolean</option><option>blank</option></select>");
+        $('.main').append("<div class='option'></div>");
+        $('.option').append("<label for='choices'>Choices:</label><br>");
+        $('.option').append("<input type='text' id='choices' disabled><br>");
+        $('.option').append("<label for='newchoice'>Add choice:</label><br>");
+        $('.option').append("<input type='text' id='choice'><br>");
+        $('.option').append("<button type='button' id='addchoice'>Add choice!</button>");
+        addChoice();
+        $('.option').append("<label for='answer'>Answer:</label>");
+        $('.option').append("<input type='text' id='answer'><br>");
+        $('.main').append("<button type='button' id='submit'>Add!</button>");
+        questionType();
+        addQuestion(quizId);
+      });
       showquestion(data[i].id);
+
+    }
+  });
+};
+var addQuestion = function(id) {
+  $('#submit').click(function(){
+    if ($('select').val() === 'multiple') {
+      var question = $('#newquestion').val();
+      var answer = $('#answer').val();
+      var quizId = id;
+      var questionType = 'multiple';
+      var choices = $('#choices').val();
+      choices = choices.substring(0,choices.length-1); 
+      $.ajax({
+        type:'POST',
+        url:'/questions',
+        data:{question:{
+          question:question,
+          answer:answer,
+          quiz_id:quizId,
+          question_type:questionType,
+          choices:choices
+        },
+        quiz_id:quizId
+      }
+      });
+    } else {
+      var question = $('#newquestion').val();
+      var answer = $('#answer').val();
+      var quizId = id;
+      var questionType = $('#submit').val();
+      $.ajax({
+        type:'POST',
+        url:'/questions',
+        data:{question:{
+          question:question,
+          answer:answer,
+          quiz_id:quizId,
+          question_type:questionType
+        },
+        quiz_id:quizId
+      }
+      });
+    }
+  });
+};
+var questionType = function() {
+  $('select').change(function(){
+    if ($('select').val() === 'blank') {
+      $('.option').empty();
+      $('.option').append("<label for='answer'>Answer:</label>");  
+      $('.option').append("<input type='text' id='answer'>");
+    } else if ($('select').val() === 'boolean') {
+      $('.option').empty();
+      $('.option').append("<label for='answer'>Answer:</label>");  
+      $('.option').append("<input type='radio' id='answer' name='answer' value='true' checked>True!<br><input type='radio' id='answer' name='answer' value='false'>False!<br>");
+    } else {
+      $('.option').empty();
+      $('.option').append("<label for='choices'>Choices:</label><br>");
+      $('.option').append("<input type='text' id='choices' disabled><br>");
+      $('.option').append("<label for='newchoice'>Add choice:</label><br>");
+      $('.option').append("<input type='text' id='choice'><br>");
+      $('.option').append("<button type='button' id='addchoice'>Add choice!</button>");
+      addChoice();
+      $('.option').append("<label for='answer'>Answer:</label>");
+      $('.option').append("<input type='text' id='answer'><br>");
+    }
+  });
+};
+var addChoice = function(){
+  $('#addchoice').click(function(){
+    if ($('#choice').val() !== "") {
+      var choices = $('#choices').val();
+      $('#choices').val(choices+$('#choice').val()+';');
+      $('#choice').val("");
     }
   });
 };
@@ -129,7 +223,9 @@ $(document).ready(function(){
         url:"/quizzes",
         data:{quiz:{title:quiz}}
       }).done(function(data){
-        console.log(data);
+        var title = data.entity.title;
+        $('.main').empty();
+        $('.main').append("<h2>Successfully create new quiz '"+title+"'");
       });
     });
   });
